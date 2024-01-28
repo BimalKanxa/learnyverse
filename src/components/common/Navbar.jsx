8a5535d7823b9307096a8621cc14ec9a3c11fe89
BimalKanxa <sharmabimal2003@gmail.com>
2024-01-28 20:34:12 +0530

    community navlink added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -64,7 +64,7 @@ function Navbar() {
           <ul className="flex md:gap-x-6 text-richblack-25">
             {NavbarLinks.map((link, index) => (
               <li key={index}>
-                {link.title === "Catalog" ? (
+                {link.title === "Courses" ? (
                   <>
                     <div
                       className={`group relative flex cursor-pointer items-center gap-1 ${
@@ -165,7 +165,7 @@ function Navbar() {
 
 {NavbarLinks.map((link, index) => (
               <li key={index} className="list-none">
-                {link.title === "Catalog" ? (
+                {link.title === "Courses" ? ( 
                   <>
                   </>
                 ) : (
