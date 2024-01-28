621733f786f00a7952e70a7ec4a0dace8c5014d9
baroonjha <85762785+baroonjha@users.noreply.github.com>
2024-01-28 22:05:45 +0530

    otp functionality is working fine

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,9 +4,16 @@ const mailSender = async (email, title, body) => {
     try{
             let transporter = nodemailer.createTransport({
                 host:process.env.MAIL_HOST,
+                service:'gmail',
+                // host:process.env.MAIL_HOST,
+                host:'smtp.gmail.com',
+                port:587,
+                secure:false,
                 auth:{
-                    user: process.env.MAIL_USER,
-                    pass: process.env.MAIL_PASS || "vwwfswipndpxdalb",
+                    // user: process.env.MAIL_USER ,
+                    // pass: process.env.MAIL_PASS,
+                    user:'baroonjha14@gmail.com',
+                    pass:'inmr xlfq wbjw gpsz',
                 }
             })
 
